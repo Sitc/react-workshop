b1bde1f61ae1ce6e9c82af45d125f9f19504e32f
Brad Westfall <brad@azpixels.com>
2022-05-04 15:03:41 -0400

    end of day 1

--- courses/core/01-thinking-in-react/lecture/index.jsx
+++ courses/core/01-thinking-in-react/lecture/index.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { FaTrash } from 'react-icons/fa'
 import './styles.scss'
 
-// This is our "entry" file for our build system.
+function Button({ children, onClick }) {
+  return (
+    <button onClick={onClick} className="button" type="button">
+      {children}
+    </button>
+  )
+}
 
-// 1. Use React to make our app
-// 2. "mount" the app to the DOM
+function App() {
+  const courses = [
+    { id: 1, name: 'HTML' },
+    { id: 2, name: 'CSS' },
+    { id: 3, name: 'JS' },
+  ]
 
-// Let's go
+  function onClick() {}
+
+  return (
+    <div>
+      {courses.map((course) => {
+        return (
+          <div key={course.id}>
+            <h1>{course.name}</h1>
+            <Button onClick={onClick}>
+              <span>Remove Course</span>
+              <FaTrash />
+            </Button>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
